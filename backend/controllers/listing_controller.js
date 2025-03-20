0691b42a184a07aae736bbd0e13df1587bf16baf
EyupCanbay <eyupcanbay40@gmail.com>
2025-03-20 12:25:13 +0300

    Added fetch all listing endpoint, routing process and update endpoints

--- backend/controllers/listing_controller.js
+++ backend/controllers/listing_controller.js
@@ -1,17 +1,17 @@
-const { LostPetListing, PetListing, Comment, ReplyComment, Notification } = require('../models/index')
+const { LostPetListing, PetListing, Comment, ReplyComment, Notification, User, Address } = require('../models/index')
 const responseHandler = require('../utils/responseHandler')
 const Enum = require('../config/enum')
 const mongoose = require('mongoose')
 
 async function createListing(req, res, next) {
-    const { selection } = req.query; // selection is boolean variable 
+    const { query } = req.query; // selection is boolean variable 
     const session = await mongoose.startSession();
     session.startTransaction();
 
     try {
         let newListing;
         
-        if ( selection ) {
+        if ( query ) {
 
             newListing = new LostPetListing({
                 user_id: req.user._id,
@@ -26,7 +26,7 @@ async function createListing(req, res, next) {
                 additionalInfo: req.body.additionalInfo
             })
 
-        } else if ( selection ) {
+        } else if ( query ) {
 
             newListing = new PetListing({
                 user_id: req.user._id,
@@ -52,7 +52,7 @@ async function createListing(req, res, next) {
         return responseHandler.success({ 
             res, 
             statusCode: Enum.HTTP_CODES.CREATED, 
-            message: `${selection} pet listing created successfully`, 
+            message: `Pet listing created successfully`, 
             data: newListing 
         });
     } catch (error) {
@@ -67,8 +67,38 @@ async function createListing(req, res, next) {
     }
 }
 
+async function getAllListing(req,res,next) {
+    try {
+        const { query, page = 1, limit = 2 } = req.query;
+
+        const skip = (Number(page) - 1) * Number(limit);
+    
+        const listingModel = query ? LostPetListing : PetListing;
+    
+        const listings = await listingModel.find()
+          .select("-comment_id")
+          .populate({
+            path: "user_id",
+            select: "userName name surname profilePhoto phoneNumber",
+            populate: {
+              path: "location",
+              model: "Address",
+              select: "country city state neighborhood"
+            }
+          })
+          .sort({ createdAt: -1 })
+          .skip(skip)
+          .limit(Number(limit));
+
+          return responseHandler.success({res, statusCode: Enum.HTTP_CODES.OK, data:listings})
+    } catch ( error ) {
+        return responseHandler.error({res, statusCode: Enum.HTTP_CODES.BAD_REQUEST, error})
+    }
+}
+
 
 
 module.exports = {
-    createListing
+    createListing,
+    getAllListing
 }
--- backend/routes/listing_routes.js
+++ backend/routes/listing_routes.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const listingController = require('../controllers/listing_controller')
 const { checkUser } = require('../middleware/auth_middleware')
 
-router.post('/',checkUser, listingController.createListing)
+router.post('/',checkUser , listingController.createListing)
+router.get('/',checkUser , listingController.getAllListing)
+
 
 
 module.exports = router
