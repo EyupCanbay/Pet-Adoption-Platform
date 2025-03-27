510f7da5e3cb9df95783fb076cecb872ed77c960
ahmetanil1 <amtyrgnc@gmail.com>
2025-03-27 16:27:14 +0300

    Add loading comp.

--- frontend/src/containers/HomePageContainer.jsx
+++ frontend/src/containers/HomePageContainer.jsx
@@ -1,18 +1,25 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import HomeComponent from "../components/HomeComponent";
+import Loading from "../components/Loading";
 
 function HomePageContainer() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
+    if (loading) {
+        return <Loading />;
+    }
 
     return (
         <div className="flex">
-            <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
-
-            <div
-                className="transition-all duration-300 ease-in-out flex-1" 
-            >
+            <Sidebar />
+            <div className="transition-all duration-300 ease-in-out flex-1">
                 <HomeComponent />
             </div>
         </div>
