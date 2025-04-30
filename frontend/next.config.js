19a2d4800e70bd36601f375919927a98ed4f30d7
EyupCanbay <eyupcanbay40@gmail.com>
2025-04-30 12:42:11 +0300

    Fix backend branch

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ["example.com"], // Buraya izin verilen domainleri ekle
-    },
-  };
-  
-  module.exports = nextConfig;
-  
