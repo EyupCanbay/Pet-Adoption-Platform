7c1004b4bfd2541908e6d00f81e8a9944f80e1c4
ahmetanil1 <amtyrgnc@gmail.com>
2025-05-06 15:36:19 +0300

    Create category endpoints

---
+++ frontend/src/services/Category.js
@@ -0,0 +1,106 @@
+const backend_url = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export const getAllCategories = async () => {
+
+    try {
+        const response = await fetch(`${backend_url}/api/categories`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.message}`);
+        }
+        const data = await response.json();
+        console.log("Categories fetched successfully:", data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching categories:", error);
+        throw error;
+    }
+}
+
+
+export const createCategory = async (formData) => {
+    try {
+        const response = await fetch(`${backend_url}/categories`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.message}`);
+        }
+        const data = await response.json();
+        console.log("Category created successfully:", data);
+        return data;
+    } catch (error) {
+        console.error("Error creating category:", error);
+        throw error;
+    }
+}
+
+export const updateCategory = async (id, formData) => {
+    try {
+        const response = await fetch(`${backend_url}/categories/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.message}`);
+        }
+        const data = await response.json();
+        console.log("Category updated successfully:", data);
+        return data;
+    } catch (error) {
+        console.error("Error updating category:", error);
+        throw error;
+    }
+}
+
+export const deleteCategory = async (id) => {
+    try {
+        const response = await fetch(`${backend_url}/categories/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.message}`);
+        }
+        const data = await response.json();
+        console.log("Category deleted successfully:", data);
+        return data;
+    } catch (error) {
+        console.error("Error deleting category:", error);
+        throw error;
+    }
+}
+
+export const createSubCategoryByCategoryId = async (categoryId, formData) => {
+    try {
+        const response = await fetch(`${backend_url}/categories/${categoryId}/subcategories`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.message}`);
+        }
+        const data = await response.json();
+        console.log("Subcategory created successfully:", data);
+        return data;
+    } catch (error) {
+        console.error("Error creating subcategory:", error);
+        throw error;
+    }
+}
