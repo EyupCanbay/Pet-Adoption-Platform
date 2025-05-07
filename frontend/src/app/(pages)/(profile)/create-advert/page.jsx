30c003c0e9b4e41988c80611a9ef914819ef35ec
ahmetanil1 <amtyrgnc@gmail.com>
2025-05-07 22:52:43 +0300

    Create a create page for listing and lost listing pets and tabbed

--- frontend/src/app/(pages)/(profile)/create-advert/page.jsx
+++ frontend/src/app/(pages)/(profile)/create-advert/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import LostListing from '@/src/components/create-advert/lost-listing'
+import Listing from '@/src/components/create-advert/listing'
 
 function CreateAdverts() {
+    const [activeTab, setActiveTab] = useState('listing')
+
+    const handleTabClick = (tab) => {
+        setActiveTab(tab)
+    }
+
+    const renderTabContent = () => {
+        switch (activeTab) {
+            case 'listing':
+                return <Listing />
+            case 'lost-listing':
+                return <LostListing />
+            default:
+                return null
+        }
+    }
+
     return (
         <div>
-            <div>CreateAdverts</div>
+            <div className="flex gap-4 mb-4">
+                <button
+                    onClick={() => handleTabClick('listing')}
+                    className={`tab ${activeTab === 'listing' ? 'active' : ''} border border-2 p-2`}
+                >
+                    Listing
+                </button>
+                <button
+                    onClick={() => handleTabClick('lost-listing')}
+                    className={`tab ${activeTab === 'lost-listing' ? 'active' : ''} border border-2 p-2`}
+                >
+                    Lost Listing
+                </button>
+            </div>
+            <div>
+                {renderTabContent()}
+            </div>
         </div>
     )
 }
---
+++ frontend/src/components/create-advert/lost-listing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LostListing() {
+    return (
+        <div>LostListing</div>
+    )
+}
+
+export default LostListing
