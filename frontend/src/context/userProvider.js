1d3760d072db136fd2974cc49292d4126c9be4b7
ahmetanil1 <amtyrgnc@gmail.com>
2025-05-22 16:32:18 +0300

    Change user provider and create wrapper for improve performance on lighthouse

--- frontend/src/context/userProvider.js
+++ frontend/src/context/userProvider.js
@@ -1,16 +1,26 @@
-"use client";
+"use client"; 
 
 import { createContext, useContext, useState, useEffect } from "react";
-
 const UserContext = createContext(null);
 
-export const useUser = () => useContext(UserContext);
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === null) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
 
-export default function UserProvider({ user, children }) {
-  const [initialUser, setInitialUser] = useState(user);
+export default function UserProvider({ user: initialUserProp, children }) {
+  const [currentUser, setCurrentUser] = useState(initialUserProp);
 
+  useEffect(() => {
+    if (initialUserProp !== currentUser) {
+      setCurrentUser(initialUserProp);
+    }
+  }, [initialUserProp, currentUser]);
   return (
-    <UserContext.Provider value={{ user, setInitialUser }}>
+    <UserContext.Provider value={{ user: currentUser, setUser: setCurrentUser }}>
       {children}
     </UserContext.Provider>
   );
